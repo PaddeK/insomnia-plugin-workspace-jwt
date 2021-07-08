a7cca37b00a86f73929d7b513dc88350bf3665d6
pkloes <kloes@adacor.com>
2021-07-08 12:35:22 +0200

    bugfix

---
+++ main.js
@@ -0,0 +1,4 @@
+module.exports.workspaceActions = require('./src/WorkspaceActions');
+module.exports.requestActions = [require('./src/RequestAction')];
+module.exports.requestHooks = [require('./src/RequestHook')];
+module.exports.responseHooks = [require('./src/ResponseHook')];
--- src/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.workspaceActions = require('./WorkspaceActions');
-module.exports.requestActions = [require('./RequestAction')];
-module.exports.requestHooks = [require('./RequestHook')];
-module.exports.responseHooks = [require('./ResponseHook')];
